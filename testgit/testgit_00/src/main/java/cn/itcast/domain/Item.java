559796e4ed019b2b2499e27866ce2629350a2c6e
Sun <178697645@qq.com>
2018-12-16 20:30:05 +0800

    第二次提交

---
+++ testgit/testgit_00/src/main/java/cn/itcast/domain/Item.java
@@ -0,0 +1,12 @@
+package cn.itcast.domain;
+
+/**
+ * @author sunxu
+ * @date 2018/12/16
+ * @describe 测试
+ */
+public class Item {
+    public  static void main(String[] args){
+        System.out.println("第一次....");
+    }
+}
